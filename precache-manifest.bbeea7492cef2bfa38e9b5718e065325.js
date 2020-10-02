4cd9e763bc0d2f290e39360dc963a9c93e057510
enable73 <enable73@gmail.com>
2020-10-02 20:32:14 +0900

    Add files via upload

---
+++ precache-manifest.bbeea7492cef2bfa38e9b5718e065325.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93e460f78223b2bcbbdc21021d294c6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71082e4146eb76d5f59d",
+    "url": "/static/css/main.379d31af.chunk.css"
+  },
+  {
+    "revision": "4a794d5942ad41dc4d89",
+    "url": "/static/js/2.d3c59976.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d3c59976.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71082e4146eb76d5f59d",
+    "url": "/static/js/main.d66490b1.chunk.js"
+  },
+  {
+    "revision": "705c69162250a325d3b6",
+    "url": "/static/js/runtime-main.f2e4f4fd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/enable73/enable73.github.io/precache-manifest.2114cedc58b2d752103b7d4402a282c8.js"
+  "/precache-manifest.bbeea7492cef2bfa38e9b5718e065325.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/enable73/enable73.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
